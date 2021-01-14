44b5379595c621cbe894e02d4d14ca816cedcde1
Mulyadi <mulyadinamrozy@gmail.com>
2021-01-14 09:49:32 +0800

    adding name to group route and caching

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::prefix('todo_api')
+    ->name('todo_api.')
     ->group(function () {
-        Route::get('/', 'ApiController@getAllToDoList');
-        Route::get('{todo:tittle}', 'ApiController@getToDoList');
-        Route::post('add', 'ApiController@createToDoList');
-        Route::put('update/{id}', 'ApiController@updateToDoList');
-        Route::delete('delete/{id}', 'ApiController@deleteToDoList');
+        Route::get('/', 'ApiController@getAllToDoList')->name('getAll');
+        Route::get('{todo:tittle}', 'ApiController@getToDoList')->name('getOne');
+        Route::post('add', 'ApiController@createToDoList')->name('add');
+        Route::put('update/{id}', 'ApiController@updateToDoList')->name('update');
+        Route::delete('delete/{id}', 'ApiController@deleteToDoList')->name('delete');
     });
