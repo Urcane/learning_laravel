733aa79c6f5adff20a598ef6bfa97c59f130232d
Mulyadi <mulyadinamrozy@gmail.com>
2021-01-14 10:47:12 +0800

    adding new traits to ToDoApiController

--- app/Http/Controllers/Api/ToDoApiController.php
+++ app/Http/Controllers/Api/ToDoApiController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Traits\ToDoApiTraits;
-use App\ToDoTables;
-use Illuminate\Http\Request;
 
 class ToDoApiController extends Controller
 {
